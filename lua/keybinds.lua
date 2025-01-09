372c8a1634f079eba6975439b4bcc63aaafb9f90
Haru <parajogapaladins@gmail.com>
2025-01-09 17:52:53 -0300

    add comment in plugins and add search for string bind

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -9,6 +9,7 @@ local telescope = require('telescope.builtin')
 -- =====================================
 -- =============# KEYBINDS #============
 -- =====================================
+map_key('n', '<C-s>',     telescope.live_grep,  {}) -- open file finder
 map_key('n', '<C-Space>', telescope.find_files, {}) -- open file finder
 
 map_key("n", "<C-j>", ":m .+1<CR>==") -- move line up(n)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,26 +4,27 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged/')
 
+-- misc
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
 Plug('andweeb/presence.nvim')
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-
-Plug('hrsh7th/cmp-buffer')
-Plug('hrsh7th/cmp-path')
-Plug('hrsh7th/cmp-cmdline')
-Plug('hrsh7th/nvim-cmp')
-
 Plug('norcalli/nvim-colorizer.lua')
-
 Plug('roobert/search-replace.nvim')
 
+-- rust specific
+Plug('saecki/crates.nvim',            { ['tag'] = 'stable' })
+Plug('simrat39/rust-tools.nvim')
 
+
+-- package managers
 Plug('williamboman/mason.nvim')
 Plug('williamboman/mason-lspconfig.nvim')
+
+-- LSP, Code Completion and Code Highlight
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 Plug('neovim/nvim-lspconfig')
-Plug('simrat39/rust-tools.nvim')
 Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-cmdline')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/cmp-nvim-lua')
 Plug('hrsh7th/cmp-nvim-lsp-signature-help')
@@ -32,10 +33,8 @@ Plug('hrsh7th/cmp-path')
 Plug('hrsh7th/cmp-buffer')                            
 Plug('hrsh7th/vim-vsnip')
 
+-- themes
 Plug('catppuccin/nvim')
 -- Plug("tiagovla/tokyodark.nvim")
-
-Plug('saecki/crates.nvim',            { ['tag'] = 'stable' })
-
 
 vim.call('plug#end')
